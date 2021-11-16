53a5fe98a44e6e3afb24c22c9b4f5c1adcd7f092
Bryant6 <1963899588@qq.com>
2021-11-16 21:30:30 +0800

    加入nacos服务注册，将service-cmn和service-hosp服务注册到nacos。localhost:8848/nacos

--- service/service_cmn/src/main/java/com/wang/yygh/cmn/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/com/wang/yygh/cmn/ServiceCmnApplication.java
@@ -2,6 +2,7 @@ package com.wang.yygh.cmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.wang"})
+@EnableDiscoveryClient
 public class ServiceCmnApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCmnApplication.class,args);
