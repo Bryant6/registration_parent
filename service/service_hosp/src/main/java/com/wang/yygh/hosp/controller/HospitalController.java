2c8bfe55da11c82c2de9a35aeadd8cd97731ce92
Bryant6 <1963899588@qq.com>
2021-11-18 13:59:28 +0800

    添加医院详情接口

--- service/service_hosp/src/main/java/com/wang/yygh/hosp/controller/HospitalController.java
+++ service/service_hosp/src/main/java/com/wang/yygh/hosp/controller/HospitalController.java
@@ -4,6 +4,8 @@ import com.wang.yygh.common.result.Result;
 import com.wang.yygh.hosp.service.HospitalService;
 import com.wang.yygh.model.hosp.Hospital;
 import com.wang.yygh.vo.hosp.HospitalQueryVo;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -28,4 +30,25 @@ public class HospitalController {
         Page<Hospital> pageModel = hospitalService.selectHospPage(page,limit,hospitalQueryVo);
         return Result.ok(pageModel);
     }
+
+    @ApiOperation(value = "更新上线状态")
+    @GetMapping("updateHospStatus/{id}/{status}")
+    public Result updateHospStatus(
+            @ApiParam(name = "id", value = "医院id", required = true)
+            @PathVariable("id") String id,
+            @ApiParam(name = "status", value = "状态（0：未上线 1：已上线）", required = true)
+            @PathVariable("status") Integer status){
+        hospitalService.updateStatus(id, status);
+        return Result.ok();
+    }
+
+    @ApiOperation(value = "获取医院详情")
+    @GetMapping("show/{id}")
+    public Result show(
+            @ApiParam(name = "id", value = "医院id", required = true)
+            @PathVariable String id) {
+        return Result.ok(hospitalService.show(id));
+    }
+
+
 }
